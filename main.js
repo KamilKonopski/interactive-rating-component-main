10da0df6b1893198a77f01972a67e9e0b84bc2b8
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-02 00:30:11 +0200

    add logic in js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('działa!');
+const rates = document.querySelectorAll('.rates');
+const submitButton = document.querySelector('.submit');
+const currentRate = document.getElementById('current-rate');
+
+let currentNumber = null;
+
+rates.forEach(rate => {
+    rate.addEventListener('click', event => {
+        const clickedNumber = event.target;
+        currentNumber = clickedNumber.innerText
+    });
+});
+
+submitButton.addEventListener('click', () => {
+    currentRate.innerText = currentNumber;
+});

d250e9f657f312f2039136a908a0151dbb185a34
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-02 14:10:57 +0200

    finish  desktop app

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const rates = document.querySelectorAll('.rate');
 const submitButton = document.querySelector('.submit');
 const currentRate = document.getElementById('current-rate');
+const activeState = document.querySelector('.active-state');
+const thankYouState = document.querySelector('.thank-you-state');
 
 let currentNumber = null;
 
+
 rates.forEach(rate => {
     rate.addEventListener('click', event => {
-        const clickedNumber = event.target;
-        currentNumber = clickedNumber.innerText
+        const clickedNumber = event.target.innerText;
+        currentNumber = clickedNumber;
     });
 });
 
 submitButton.addEventListener('click', () => {
     currentRate.innerText = currentNumber;
+
+    activeState.style.display = 'none';
+    thankYouState.style.display = "flex";
 });
